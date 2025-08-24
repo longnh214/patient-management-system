46cbc9c5f630c07a06d3ffa11b1b87fb2503c76c
Nakhoon <cnh6123@naver.com>
2025-08-24 17:14:53 +0900

    [INIT] patient, visit에 대한 dto 초기 설정

--- src/main/java/com/example/patient/dto/PatientDto.java
+++ src/main/java/com/example/patient/dto/PatientDto.java
@@ -1,4 +1,82 @@
 package com.example.patient.dto;
 
+import com.example.patient.entity.Patient;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
 public class PatientDto {
+    @Getter
+    @Builder
+    public static class Request {
+        private Long hospitalId;
+        private String name;
+        private String registrationNumber;
+        private String genderCode;
+        private String birthDate;
+        private String mobilePhoneNumber;
+
+        public static Request toDto(Patient entity) {
+            return Request.builder()
+                    .hospitalId(entity.getHospital().getId())
+                    .name(entity.getName())
+                    .registrationNumber(entity.getRegistrationNumber())
+                    .genderCode(entity.getGenderCode().name())
+                    .birthDate(entity.getBirthDate() != null ? entity.getBirthDate().toString() : null)
+                    .mobilePhoneNumber(entity.getMobilePhoneNumber())
+                    .build();
+        }
+    }
+
+    @Getter
+    @Builder
+    public static class Response {
+        private Long id;
+        private Long hospitalId;
+        private String name;
+        private String registrationNumber;
+        private String genderCode;
+        private String birthDate;
+        private String mobilePhoneNumber;
+
+        public static Response toDto(Patient entity) {
+            return Response.builder()
+                    .id(entity.getId())
+                    .hospitalId(entity.getHospital().getId())
+                    .name(entity.getName())
+                    .registrationNumber(entity.getRegistrationNumber())
+                    .genderCode(entity.getGenderCode().name())
+                    .birthDate(entity.getBirthDate() != null ? entity.getBirthDate().toString() : null)
+                    .mobilePhoneNumber(entity.getMobilePhoneNumber())
+                    .build();
+        }
+    }
+
+    @Getter
+    @Builder
+    public static class Update {
+        private Long id;
+        private String name;
+        private String genderCode;
+        private String birthDate;
+        private String mobilePhoneNumber;
+
+        public static Update toDto(Patient entity) {
+            return Update.builder()
+                    .id(entity.getId())
+                    .name(entity.getName())
+                    .genderCode(entity.getGenderCode().name())
+                    .birthDate(entity.getBirthDate() != null ? entity.getBirthDate().toString() : null)
+                    .mobilePhoneNumber(entity.getMobilePhoneNumber())
+                    .build();
+        }
+    }
+
+    @Builder
+    @Getter
+    public static class PatientDetailResponse {
+        private PatientDto.Response patient;
+        private List<VisitDto.Response> visits;
+    }
 }
--- src/main/java/com/example/patient/dto/VisitDto.java
+++ src/main/java/com/example/patient/dto/VisitDto.java
@@ -1,4 +1,61 @@
 package com.example.patient.dto;
 
+import com.example.patient.entity.Visit;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
 public class VisitDto {
+    @Getter
+    @Builder
+    public static class Request {
+        private Long hospitalId;
+        private Long patientId;
+        private LocalDateTime receptionDateTime;
+        private String visitStatusCode;
+
+        public static Request toDto(Visit entity) {
+            return Request.builder()
+                    .hospitalId(entity.getHospital().getId())
+                    .patientId(entity.getPatient().getId())
+                    .receptionDateTime(entity.getReceptionDateTime())
+                    .visitStatusCode(entity.getVisitStatusCode().name())
+                    .build();
+        }
+    }
+
+    @Getter
+    @Builder
+    public static class Response {
+        private Long patientVisitId;
+        private Long hospitalId;
+        private Long patientId;
+        private LocalDateTime receptionDateTime;
+        private String visitStatusCode;
+
+        public static Response toDto(Visit entity) {
+            return Response.builder()
+                    .patientVisitId(entity.getPatientVisitId())
+                    .hospitalId(entity.getHospital().getId())
+                    .patientId(entity.getPatient().getId())
+                    .receptionDateTime(entity.getReceptionDateTime())
+                    .visitStatusCode(entity.getVisitStatusCode().name())
+                    .build();
+        }
+    }
+
+    @Getter
+    @Builder
+    public static class Update {
+        private Long patientVisitId;
+        private String visitStatusCode;
+
+        public static Update toDto(Visit entity) {
+            return Update.builder()
+                    .patientVisitId(entity.getPatientVisitId())
+                    .visitStatusCode(entity.getVisitStatusCode().name())
+                    .build();
+        }
+    }
 }
