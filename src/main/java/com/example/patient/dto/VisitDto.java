fb172fe480f51039342a146ee15d9a246913fb03
Nakhoon <cnh6123@naver.com>
2025-08-24 17:12:26 +0900

    [INIT] hospital, patient, visit에 대한 repository 초기 설정

---
+++ src/main/java/com/example/patient/dto/VisitDto.java
@@ -0,0 +1,4 @@
+package com.example.patient.dto;
+
+public class VisitDto {
+}
---
+++ src/main/java/com/example/patient/repository/VisitRepository.java
@@ -0,0 +1,9 @@
+package com.example.patient.repository;
+
+import com.example.patient.entity.Visit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VisitRepository extends JpaRepository<Visit, Long> {
+}
