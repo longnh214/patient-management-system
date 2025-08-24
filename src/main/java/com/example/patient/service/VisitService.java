eb2ed625d01c1d771af9b7e189840711ac3ca007
Nakhoon <cnh6123@naver.com>
2025-08-24 20:04:26 +0900

    [INIT] 방문 관련 서비스 추가

--- src/main/java/com/example/patient/service/VisitService.java
+++ src/main/java/com/example/patient/service/VisitService.java
@@ -1,4 +1,73 @@
 package com.example.patient.service;
 
+import com.example.patient.code.VisitStatusCode;
+import com.example.patient.dto.VisitDto;
+import com.example.patient.entity.Hospital;
+import com.example.patient.entity.Patient;
+import com.example.patient.entity.Visit;
+import com.example.patient.repository.HospitalRepository;
+import com.example.patient.repository.PatientRepository;
+import com.example.patient.repository.VisitRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
 public class VisitService {
+    private final VisitRepository visitRepository;
+    private final HospitalRepository hospitalRepository;
+    private final PatientRepository patientRepository;
+
+    @Transactional(readOnly = true)
+    public List<VisitDto.Response> getVisits(Long id){
+        List<Visit> visits = visitRepository.findAllByPatientId(id);
+        List<VisitDto.Response> visitDtos = visits.stream()
+                .map(VisitDto.Response::toDto)
+                .collect(Collectors.toList());
+
+        return visitDtos;
+    }
+
+    @Transactional
+    public VisitDto.Response create(VisitDto.Request dto) {
+        Hospital hospital = hospitalRepository.findById(dto.getHospitalId())
+                .orElseThrow(() -> new IllegalArgumentException("Hospital not found"));
+        Patient patient = patientRepository.findById(dto.getPatientId())
+                .orElseThrow(() -> new IllegalArgumentException("Patient not found"));
+
+        Visit visit = Visit.builder()
+                .hospital(hospital)
+                .patient(patient)
+                .receptionDateTime(dto.getReceptionDateTime())
+                .visitStatusCode(VisitStatusCode.valueOf(dto.getVisitStatusCode()))
+                .build();
+
+        return VisitDto.Response.toDto(visitRepository.save(visit));
+    }
+
+    @Transactional(readOnly = true)
+    public VisitDto.Response read(Long patientVisitId) {
+        Visit visit = visitRepository.findById(patientVisitId)
+                .orElseThrow(() -> new IllegalArgumentException("Visit not found"));
+        return VisitDto.Response.toDto(visit);
+    }
+
+    @Transactional
+    public VisitDto.Response update(VisitDto.Update dto) {
+        Visit visit = visitRepository.findById(dto.getPatientVisitId())
+                .orElseThrow(() -> new IllegalArgumentException("Visit not found"));
+        visit.setVisitStatusCode(VisitStatusCode.valueOf(dto.getVisitStatusCode()));
+        return VisitDto.Response.toDto(visit);
+    }
+
+    @Transactional
+    public void delete(Long patientVisitId) {
+        Visit visit = visitRepository.findById(patientVisitId)
+                .orElseThrow(() -> new IllegalArgumentException("Visit not found"));
+        visitRepository.delete(visit);
+    }
 }
