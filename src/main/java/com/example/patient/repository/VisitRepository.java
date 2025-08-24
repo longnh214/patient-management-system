967660ca6672563ab81973f6897011e4a2223bab
Nakhoon <cnh6123@naver.com>
2025-08-24 20:03:27 +0900

    [UPDATE] 환자 별 방문 이력을 위한 visit repository 수정

--- src/main/java/com/example/patient/repository/VisitRepository.java
+++ src/main/java/com/example/patient/repository/VisitRepository.java
@@ -4,6 +4,9 @@ import com.example.patient.entity.Visit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface VisitRepository extends JpaRepository<Visit, Long> {
+    List<Visit> findAllByPatientId(Long patientId);
 }
