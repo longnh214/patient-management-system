4ef8ab1f1680caf860278ce0c4351267e4d60da7
Nakhoon <cnh6123@naver.com>
2025-08-26 16:31:10 +0900

    [UPDATE] 진료 유형 코드 이름 변경

--- src/main/java/com/example/patient/code/TreatmentTypeCode.java
+++ src/main/java/com/example/patient/code/TreatmentTypeCode.java
@@ -5,18 +5,18 @@ import lombok.Getter;
 import java.util.Arrays;
 
 @Getter
-public enum TreatmentCode {
+public enum TreatmentTypeCode {
     D("약처방"),
     T("검사");
 
     private final String description;
 
-    TreatmentCode(String description) {
+    TreatmentTypeCode(String description) {
         this.description = description;
     }
 
-    public static TreatmentCode fromCode(String code) {
-        return Arrays.stream(TreatmentCode.values())
+    public static TreatmentTypeCode fromCode(String code) {
+        return Arrays.stream(TreatmentTypeCode.values())
                 .filter(g -> g.name().equalsIgnoreCase(code))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("알 수 없는 진료 유형 코드입니다: " + code));
