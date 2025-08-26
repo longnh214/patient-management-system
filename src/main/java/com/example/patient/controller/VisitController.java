af01428346b7b8215b1142fc894b273a4f4e49e8
Nakhoon <cnh6123@naver.com>
2025-08-26 15:53:06 +0900

    [INIT] visit CRUD 기반 rest controller 초기 설정

--- src/main/java/com/example/patient/controller/VisitController.java
+++ src/main/java/com/example/patient/controller/VisitController.java
@@ -1,4 +1,60 @@
 package com.example.patient.controller;
 
+import com.example.patient.dto.VisitDto;
+import com.example.patient.service.VisitService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/visit")
 public class VisitController {
+    private final VisitService visitService;
+
+    @PostMapping
+    public ResponseEntity<VisitDto.Response> create(@RequestBody VisitDto.Request request) {
+        VisitDto.Response responseDto = visitService.create(request);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(responseDto.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).body(responseDto);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<VisitDto.Response> update(@PathVariable Long id, @RequestBody VisitDto.Update updateDto) {
+        VisitDto.Update updateObj = VisitDto.Update.builder()
+                .id(id)
+                .visitStatusCode(updateDto.getVisitStatusCode())
+                .build();
+
+        VisitDto.Response responseDto = visitService.update(updateObj);
+        return ResponseEntity.ok(responseDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        visitService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<VisitDto.Response> read(@PathVariable Long id) {
+        VisitDto.Response responseDto = visitService.read(id);
+        return ResponseEntity.ok(responseDto);
+    }
+
+    @GetMapping("/patient/{patientId}")
+    public ResponseEntity<List<VisitDto.Response>> list(@PathVariable Long patientId) {
+        List<VisitDto.Response> visits = visitService.getVisits(patientId);
+        return ResponseEntity.ok(visits);
+    }
 }
