01de9de443b872464f8ae51408f775e26eed84c4
Nakhoon <cnh6123@naver.com>
2025-09-09 19:50:52 +0900

    [UPDATE] PageInfo 객체 내 생성자 관련 lombok 어노테이션 제거(불필요)

--- src/main/java/com/example/patient/constant/PageInfo.java
+++ src/main/java/com/example/patient/constant/PageInfo.java
@@ -3,8 +3,6 @@ package com.example.patient.constant;
 import lombok.*;
 
 @Getter
-@AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder
 public class PageInfo {
     int pageNo;
